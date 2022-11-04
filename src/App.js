fb7545d6e7fedd6db71490d7460d67aa448410b6
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2022-11-04 10:35:07 -0500

    Finished project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [total, setTotal] = useState(0)
+
+  const add = (e) => {
+    e.preventDefault()
+    let currentTotal = document.querySelector('#num').value
+    if (currentTotal === '') return;
+    let sum = total + parseInt(currentTotal)
+    setTotal(sum)
+    document.querySelector('#num').value = ''
+  }
+
+  const subtract = e => {
+    e.preventDefault()
+    let currentTotal = document.querySelector('#num').value
+    if (currentTotal === '') return;
+    let sum = total - parseInt(currentTotal)
+    setTotal(sum)
+    document.querySelector('#num').value = ''
+  }
+
+  const multiply = e => {
+    e.preventDefault()
+    let currentTotal = document.querySelector('#num').value
+    if (currentTotal === '') return;
+    let sum = total * parseInt(currentTotal)
+    setTotal(sum)
+    document.querySelector('#num').value = ''
+  }
+
+  const divide = e => {
+    e.preventDefault()
+    let currentTotal = document.querySelector('#num').value
+    if (currentTotal === '') return;
+    let sum = total / parseInt(currentTotal)
+    setTotal(sum)
+    document.querySelector('#num').value = ''
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Simplest Working Calculator</h1>
+      <p>{total}</p>
+      <input type='text' id='num' placeholder='0' />
+      <button type='button' onClick={add}>Add</button>
+      <button type='button' onClick={subtract}>Subtract</button>
+      <button type='button' onClick={multiply}>Multiply</button>
+      <button type='button' onClick={divide}>Divide</button>
+      <button type='button' onClick={() => document.querySelector('#num').value = ''}>Reset Input</button>
+      <button type='button' onClick={() => setTotal(0)}>Reset Result</button>
+    </div >
   );
 }
 
